99a780a4e4efc14a2f670f9ff898b76ccf6a9ddf
spliced0 <54321928+placidseven@users.noreply.github.com>
2022-07-01 21:07:32 +0000

    myapps

--- myApp.js
+++ myApp.js
@@ -1,29 +1,31 @@
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
-let absolutePath = __dirname + "/views/index.html"
-let middleware = express.static(__dirname + "/public")
+//Middleware are functions that intercept route handlers, adding some kind of information. A middleware needs to be mounted using the method app.use(path, middlewareFunction). The first path argument is optional. If you don’t pass it, the middleware will be executed for all requests.
+//An HTML server usually has one or more directories that are accessible by the user. You can place the static assets needed by your application (stylesheets, scripts, images) there.
+//In Express, you can put in place this functionality using the middleware express.static(path), where the path parameter is the absolute path of the folder containing the assets.
+let absolutePath = express.static(__dirname + "/public")
+app.use("/public", absolutePath);
 
-console.log("Hello World")
-
-app.use(function middleware(req, res, next) {
+//Middleware functions are functions that take 3 arguments: the request object, the response object, and the next function in the application’s request-response cycle. These functions execute some code that can have side effects on the app, and usually add information to the request or response objects. They can also end the cycle by sending a response when some condition is met. If they don’t send the response when they are done, they start the execution of the next function in the stack. This triggers calling the 3rd argument, next().
+app.use(function (req, res, next) {
   console.log(req.method +" "+ req.path +" - "+ req.ip);
   next();
 });
 
-// respond with "hello world" when a GET request is made to the homepage
-app.get('/', (req, res) => {
-  //res.send('Hello Express')
-  res.sendFile(absolutePath)
-});
-
-app.use("/public", middleware);
-
-app.get('/now', function middleware(req, res, next) {
+//Middleware can be mounted at a specific route using app.METHOD(path, middlewareFunction). Middleware can also be chained within a route definition.
+app.get('/now', function (req, res, next) {
   req.time = new Date().toString()
   next();
 }, function(req, res) {
   res.json({time: req.time});
+});
+
+absolutePath = __dirname + "/views/index.html"
+app.get('/', (req, res) => {
+  //res.send('Hello Express')
+  res.sendFile(absolutePath)
 });
 
 app.get('/json', (req, res) => {
@@ -39,12 +41,14 @@ app.get('/:word/echo', (req, res) => {
   res.json({echo: req.params.word});
 });
 
-app.get("/name", function(req, res) {
+app.get("/name", (req, res) => {
     //res.json({name: req.query.first});
   res.json({name: `${req.query.first} ${req.query.last}`});
 });
 
-
+app.post("/name", function(req, res){
+  res.json({name: `${req.body.first} ${req.body.last}`});
+});
 
 
 
